629145a62de887f0efb6c2e8ecb59dce69fc5703
ilyasaitbel <ilyasilya084@gmail.com>
2025-09-02 15:21:50 +0200

    ex 3

--- c/main.c
+++ c/main.c
@@ -2,14 +2,11 @@
 # include <math.h>
 int main(){
 
-    float r = 10;
+    float rayon = 10;
     const double p = 3.1415926535897;
     float volume;
-    
-
-
-    volume = (4.0f/3.0f)*p*pow(r,3);
-     printf("la valeur du volume et : %f",volume);
-    
-
+    printf("entre la valeur du rayon : ");
+    scanf("%f",&rayon);
+    volume = (4.0f/3.0f)*p*pow(rayon,3);
+    printf("la valeur du volume et : %f",volume);
 }
