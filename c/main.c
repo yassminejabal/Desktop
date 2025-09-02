17094d26ca8306b71dabe9877441a6e3d712ac8f
ilyasaitbel <ilyasilya084@gmail.com>
2025-09-02 13:04:16 +0200

    ex1

---
+++ c/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    printf("1.          *\n");
+    printf("2.         *\n");
+    printf("3.        *\n");
+    printf("4. *     *\n");
+    printf("5.  *   *\n");
+    printf("6.    *");
+
+return 0;
+}
