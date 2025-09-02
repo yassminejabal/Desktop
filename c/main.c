bdd407191b1f26a51019b5cf869bdb839be5d9af
yassmine <jabalyassmine2007@gmail.com>
2025-09-02 17:54:41 +0200

    ex 4

--- c/main.c
+++ c/main.c
@@ -2,12 +2,6 @@
 # include <math.h>
 int main()
 {
-    float montont,taxe,la_valeur_final;
-
-    taxe=0.05;
-    printf("entrer le montont: ");
-    scanf("%f",&montont);
-    la_valeur_final = (montont * taxe) + montont;
-    printf("la valeur du montont avec le taxe est :%f",la_valeur_final);
+   
     return 0;
 }
