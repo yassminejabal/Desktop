77516ac4d42dad2f1f40c5a4c365a26110727187
yassmine <jabalyassmine2007@gmail.com>
2025-09-03 11:37:24 +0200

    ex6

--- c/main.c
+++ c/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 # include <math.h>
 int main()
-{
-   
+int x,polynome,s;
+    printf("entre la valeur de x : ");
+    scanf("%d",&x);
+    s = ((((3*x+2)x–5)x–1)x+7)x–6;
+    printf("la valeur de s est : %d",s);
     return 0;
 }
